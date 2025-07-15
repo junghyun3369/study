bb28d6fda081379327d428990399107822a42f4f
junghyun3369 <leesang3369@gmail.com>
2025-07-15 15:13:23 +0900

    수터디

---
+++ appp.js
@@ -0,0 +1,89 @@
+var a = '변수값' ;
+// console.log(a); // print(a)
+
+let b = 1
+b = 2
+//console.log(b)
+
+const c = 5
+//  c = 10
+//  console.log(c)
+
+let d = 10
+// console.log(d, typeof d)    
+d = '문자'  
+// console.log(d, typeof d)
+
+if(1 == 0) {
+    console.log("참")
+} else {
+    console.log("거짓")
+}
+
+if(1 == 0) {
+//  console.log("참")
+} else {
+  //console.log("거짓")
+}
+
+if(1 == 0) {
+  //console.log("참")
+} else {
+ // console.log("거짓")
+}
+
+if(false) {
+ // console.log("참")
+} else {
+ // console.log("거짓")
+}
+
+
+if(true) {
+ // console.log("참")
+} else {
+//  console.log("거짓")
+}
+
+console.log("콜라, 사이다, 환타 중 하나를 주문하세요.")
+음료 = '환타'
+금액 = 0
+if(음료 == '콜라') {
+    console.log('콜라가 죽었다. 500원')
+    금액 = 500
+}else if(음료 == '사이다') {
+    console.log('사이다는 돌아오지 않았다. 450원')
+    금액 = 450
+}else if(음료 == '환타') {
+    console.log('환타는 숨을 거두었다. 300원')
+    금액 = 300
+}
+console.log(음료 + '의 가격은 ' + 금액)
+
+음료 = '콜라'
+금액 = 0
+if(음료 == '콜라') {
+    console.log('콜라가 죽었다. 500원')
+    금액 = 500
+}else if(음료 == '사이다') {
+    console.log('사이다는 돌아오지 않았다. 450원')
+    금액 = 450
+}else if(음료 == '환타') {
+    console.log('환타는 숨을 거두었다. 300원')
+    금액 = 300
+}
+console.log(음료 + '의 가격은 ' + 금액)
+
+음료 = '사이다'
+금액 = 0
+if(음료 == '콜라') {
+    console.log('콜라가 죽었다. 500원')
+    금액 = 500
+}else if(음료 == '사이다') {
+    console.log('사이다는 돌아오지 않았다. 450원')
+    금액 = 450
+}else if(음료 == '환타') {
+    console.log('환타는 숨을 거두었다. 300원')
+    금액 = 300
+}
+console.log(음료 + '의 가격은 ' + 금액)
